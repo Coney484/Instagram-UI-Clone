62f22d6364d0c4d3442d7c56fb9ae5c9f1738f49
ROHAN CHOUDHARY <anandc.rohan484@gmail.com>
2020-12-04 21:06:21 +0530

    dummy data

---
+++ lib/constant/post_json.dart
@@ -0,0 +1,90 @@
+List posts = [
+  {
+     "id" : 1,
+    "name" : "panhchaneath_ung",
+    "profileImg" : "https://images.unsplash.com/photo-1525879000488-bff3b1c387cf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "postImg" : "https://images.unsplash.com/photo-1525879000488-bff3b1c387cf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "caption" : " SERIOUS MODE during SERIOUS RAINY DAY",
+    "isLoved" : true,
+    "commentCount" : "10",
+    "likedBy" : "whereavygoes",
+    "timeAgo" : "1 day ago"
+  },
+  {
+    "id" : 2,
+    "name" : "whereavygoes",
+    "profileImg" : "https://images.unsplash.com/photo-1512276926457-a2f9c7ec55b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzZ8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "postImg" : "https://images.unsplash.com/photo-1512276926457-a2f9c7ec55b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzZ8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "caption" : " SERIOUS MODE during SERIOUS RAINY DAY",
+    "isLoved" : true,
+    "commentCount" : "10",
+    "likedBy" : "sonitakhoun",
+    "timeAgo" : "1 day ago"
+  },
+  {
+    "id" : 3,
+    "name" : "allef_vinicius",
+    "profileImg" : "https://images.unsplash.com/photo-1545912452-8aea7e25a3d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    "postImg" : "https://images.unsplash.com/photo-1578616070222-50c4de9d5ade?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    "caption" : " It is a good to day",
+    "isLoved" : false,
+    "commentCount" : "60",
+    "likedBy" : "babysweetiepie",
+    "timeAgo" : "3 day ago"
+  },
+  {
+    "id" : 4,
+    "name" : "babysweetiepie",
+    "profileImg" : "https://images.unsplash.com/photo-1574786200654-57a80e1292b1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "postImg" : "https://images.unsplash.com/photo-1574786200654-57a80e1292b1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "caption" : "I love how a 6-year-old kid took better photos than many of my friends out there 😂",
+    "isLoved" : false,
+    "commentCount" : "70",
+    "likedBy" : "sonitakhoun",
+    "timeAgo" : "3 day ago"
+  },
+  {
+     "id" : 5,
+    "name" : "panhchaneath_ung",
+    "profileImg" : "https://images.unsplash.com/photo-1525879000488-bff3b1c387cf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "postImg" : "https://images.unsplash.com/photo-1525879000488-bff3b1c387cf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "caption" : " SERIOUS MODE during SERIOUS RAINY DAY",
+    "isLoved" : true,
+    "commentCount" : "10",
+    "likedBy" : "whereavygoes",
+    "timeAgo" : "1 day ago"
+  },
+  {
+    "id" : 6,
+    "name" : "whereavygoes",
+    "profileImg" : "https://images.unsplash.com/photo-1512276926457-a2f9c7ec55b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzZ8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "postImg" : "https://images.unsplash.com/photo-1512276926457-a2f9c7ec55b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzZ8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "caption" : " SERIOUS MODE during SERIOUS RAINY DAY",
+    "isLoved" : true,
+    "commentCount" : "10",
+    "likedBy" : "sonitakhoun",
+    "timeAgo" : "1 day ago"
+  },
+  {
+    "id" : 7,
+    "name" : "allef_vinicius",
+    "profileImg" : "https://images.unsplash.com/photo-1545912452-8aea7e25a3d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    "postImg" : "https://images.unsplash.com/photo-1578616070222-50c4de9d5ade?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    "caption" : " It is a good to day",
+    "isLoved" : false,
+    "commentCount" : "60",
+    "likedBy" : "babysweetiepie",
+    "timeAgo" : "3 day ago"
+  },
+  {
+    "id" : 8,
+    "name" : "babysweetiepie",
+    "profileImg" : "https://images.unsplash.com/photo-1574786200654-57a80e1292b1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "postImg" : "https://images.unsplash.com/photo-1574786200654-57a80e1292b1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2ZpbGUlMjBpbWFnZXN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "caption" : "I love how a 6-year-old kid took better photos than many of my friends out there 😂",
+    "isLoved" : false,
+    "commentCount" : "70",
+    "likedBy" : "sonitakhoun",
+    "timeAgo" : "3 day ago"
+  }
+];
---
+++ lib/constant/story_json.dart
@@ -0,0 +1,35 @@
+// user prfile
+String profile =
+    "https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+String name = "panhchaneath_ung";
+
+// stories
+List stories = [
+  {
+    "id": 1,
+    "img": "https://images.pexels.com/photos/1852482/pexels-photo-1852482.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "name": "sonitakhoun"
+  },
+  {
+    "id": 2,
+    "img": "https://images.pexels.com/photos/2756842/pexels-photo-2756842.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "name": "babysweetiepie"
+  },
+  {
+    "id": 3,
+    "img": "https://images.pexels.com/photos/2519829/pexels-photo-2519829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "name": "whereavygoes"
+  },
+  {
+    "id": 4,
+    "img":
+        "https://images.unsplash.com/photo-1525879000488-bff3b1c387cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    "name": "collins_lesulie"
+  },
+  {
+    "id": 5,
+    "img":
+        "https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    "name": "tyler_nix"
+  }
+];
