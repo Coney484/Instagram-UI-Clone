e59fc1ea13a0f77334ddc9830042d63b2f24715b
ROHAN CHOUDHARY <anandc.rohan484@gmail.com>
2020-12-04 21:06:52 +0530

    home_page ui

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,112 @@
+import 'package:InstagramUIClone/constant/post_json.dart';
+import 'package:InstagramUIClone/constant/story_json.dart';
+import 'package:InstagramUIClone/theme/colors.dart';
+import 'package:InstagramUIClone/widgets/post_item.dart';
+import 'package:InstagramUIClone/widgets/story_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:line_icons/line_icons.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return getBody();
+  }
+
+  Widget getBody() {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                      right: 20.0, left: 15.0, bottom: 10.0),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        width: 65,
+                        height: 65,
+                        child: Stack(
+                          children: <Widget>[
+                            Container(
+                              width: 65,
+                              height: 65,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    image: NetworkImage(profile),
+                                    fit: BoxFit.cover,
+                                  )),
+                            ),
+                            Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Container(
+                                  width: 19.0,
+                                  height: 19.0,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle, color: white),
+                                  child: Icon(Icons.add_circle,
+                                      color: buttonFollowColor, size: 19.0),
+                                ))
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      SizedBox(
+                        width: 70.0,
+                        child: Text(
+                          name,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            color: white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: List.generate(stories.length, (index) {
+                    return StoryItem(
+                      img: stories[index]['img'],
+                      name: stories[index]['name'],
+                    );
+                  }),
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            color: white.withOpacity(0.4),
+          ),
+          Column(
+            children: List.generate(posts.length, (index) {
+              return PostItem(
+                postImg: posts[index]['postImg'],
+                profileImg: posts[index]['profileImg'],
+                name: posts[index]['name'],
+                caption: posts[index]['caption'],
+                isLoved: posts[index]['isLoved'],
+                viewCount: posts[index]['commentCount'],
+                likedBy: posts[index]['likedBy'],
+                dayAgo: posts[index]['dayAgo'],
+              );
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
